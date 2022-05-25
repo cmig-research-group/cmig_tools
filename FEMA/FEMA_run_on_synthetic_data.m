40850e665027b7f4c8183b605196ae495e794b3f
Oleksandr Frei <oleksandr.frei@gmail.com>
2022-05-25 15:51:37 +0200

    simulate 'A' effect in FEMA_run_on_synthetic_data.m

--- FEMA/FEMA_run_on_synthetic_data.m
+++ FEMA/FEMA_run_on_synthetic_data.m
@@ -1,23 +1,52 @@
 % This 'hello world' demo example generates an artificial data with
-% n=10000 observations, up to 3 measures per subject, up to 2  subjects
+% n=30000 observations, up to 5 measures per subject, up to 5 subjects
 % per family. 
 %
-% To run this example clone the following repos and add them to MATLAB path
-% https://github.com/andersonwinkler/PALM 
-% https://github.com/cmig-research-group/cmig_utils
+% To run this example, clone the following repos and add them to
+% MATLAB path with 'addpath' command.
+%
+% - https://github.com/cmig-research-group/cmig_utils
+% - https://github.com/andersonwinkler/PALM   
+%   (PALM is optional - you only needed it if you play with restricted exchangeability blocks)
+
 
 % setup parameters for FEMA_synthesize
-n = 10000;
+n = 30000;
 p = 5;
 v = 10;
 
 X = randn(n, p);
-iid = sort(randsample([1:n, 1:n, 1:n], n));  % up to 3 observations per individual
-fid = ceil(iid / 2);                         % up to 2 individuals per family
+iid_int = sort(randsample([1:n, 1:n, 1:n, 1:n, 1:n], n));  % up to 5 observations per individual
+fid_int = ceil(iid_int / 5);                     % up to 5 individuals per family
+iid = cell(size(iid_int)); for i=1:length(iid_int), iid{i}=sprintf('I%i', iid_int(i)); end
+fid = cell(size(fid_int)); for i=1:length(fid_int), fid{i}=sprintf('F%i', fid_int(i)); end
+clear('iid_int'); clear('fid_int');
+
 eid = ones(n, 1);                            % non needed
 agevec = zeros(n, 1);                        % not needed
 ymat = nan(n, v);   % only size of ymat is used by FEMA_synthesize
-[ymat, sig2tvec_true, sig2mat_true] = FEMA_synthesize(X,iid,fid,agevec,ymat);
+
+% pihatmat is a square matrix with size matching the number of individuals (not the number of observations)
+[~, IA] = unique(iid, 'stable');
+pihatmat  = eye(length(IA));
+[~, ~, IC_fam] = unique(fid(IA), 'stable'); nfam=max(IC_fam);
+for fi = 1:nfam
+  idxvec = rowvec(find(IC_fam==fi)); % Identify all subjects for a given family
+  t=triu(0.5 + 0.05 * randn(length(idxvec)), 1); 
+  pihatmat(idxvec, idxvec) = pihatmat(idxvec, idxvec) + t + t';
+end
+%histogram(pihatmat(pihatmat>0 & pihatmat <1))
+
+% It's hard to distinguish 'F' vs 'A' effect, so with FASE model error bars can be fairly large
+% (though 'F'+'A' effect is well determined). Alternatively, use {'F','S','E'} model.
+RandomEffects = {'F', 'A', 'S', 'E'};
+%RandomEffects = {'F', 'S', 'E'}; 
+[~, f_effect_idx] = ismember('F', RandomEffects);
+[~, a_effect_idx] = ismember('A', RandomEffects);
+[~, s_effect_idx] = ismember('S', RandomEffects);
+[~, e_effect_idx] = ismember('E', RandomEffects);
+
+[ymat, sig2tvec_true, sig2mat_true] = FEMA_synthesize(X,iid,eid,fid,agevec,ymat,pihatmat,'RandomEffects',RandomEffects);
 
 % output of FEMA_FEMA_synthesize :
 % sig2tvec_true      - total variance of random effects, one value per voxel
@@ -27,27 +56,33 @@ ymat = nan(n, v);   % only size of ymat is used by FEMA_synthesize
 
 % setup parameters for FEMA_fit
 contrasts = ones(1, p);
-pihatmat  = eye(n);
-RandomEffects = {'F', 'S', 'E'}; % 'A'
+
 niter=1;
 nbins=20;
 
-[beta_hat, beta_se, zmat, logpmat, sig2tvec, sig2mat, binvec, logLikvec, ...
- beta_hat_perm, beta_se_perm, zmat_perm, sig2tvec_perm, sig2mat_perm, logLikvec_perm, perms] = FEMA_fit( ...
-    X,iid,eid,fid,agevec,ymat,niter,contrasts,nbins,pihatmat, 'RandomEffects', RandomEffects);
+[beta_hat,      beta_se,      zmat, logpmat, sig2tvec,      sig2mat, binvec, logLikvec, ...
+ beta_hat_perm, beta_se_perm, zmat_perm,     sig2tvec_perm, sig2mat_perm, logLikvec_perm] = FEMA_fit( ...
+    X,iid,eid,fid,agevec,ymat,niter,contrasts,nbins,pihatmat, ...
+    'RandomEffects', RandomEffects, 'nperms', 20);
 
-% draw plots to illustrate that variance components are correctly recovered
-figure(1); hold on;
+figure(1); clf; hold on;
+plot([sig2tvec_true; sig2tvec_true], [min(sig2tvec_perm, [], 3); max(sig2tvec_perm, [], 3)], 'k.-');
 plot(sig2tvec_true, sig2tvec, '*');
 plot(sig2tvec_true, sig2tvec_true, '-')
-xlabel('sig2tvec_true'); ylabel('sig2tvec');
+xlabel('sig2tvec(true)'); ylabel('sig2tvec');
 
-figure(2); hold on;
-plot(sig2mat_true(1, :), sig2mat(1, :), '*');
-plot(sig2mat_true(1, :), sig2mat_true(1, :), '-')
-xlabel('sig2mat_true, F'); ylabel('sig2mat, F');
-
-figure(3); hold on;
-plot(sig2mat_true(2, :), sig2mat(2, :), '*');
-plot(sig2mat_true(2, :), sig2mat_true(2, :), '-')
-xlabel('sig2mat_true, S'); ylabel('sig2mat, S');
+effect_idx_vec = [f_effect_idx, a_effect_idx, s_effect_idx, e_effect_idx];
+for figure_idx=1:4
+    figure(figure_idx+1); clf; hold on;
+    effect_idx = effect_idx_vec(figure_idx);
+    if effect_idx
+        x = sig2mat_true(effect_idx, :);
+        y = sig2mat(effect_idx, :);
+        y_perm = sig2mat_perm(effect_idx, :, :);
+        plot([x; x], [min(y_perm, [], 3); max(y_perm, [], 3)], 'k.-');
+        plot(x, y, '*');
+        plot(x, x, '-');
+        xlabel(sprintf('sig2mat(true), %s', RandomEffects{effect_idx}));
+        ylabel(sprintf('sig2mat, %s', RandomEffects{effect_idx}));
+    end
+end
