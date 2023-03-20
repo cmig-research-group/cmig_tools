0bb77d4c1ee360f9c7af8fe075d6a87e128d9343
Diana Smith <dmysmith@gmail.com>
2023-03-20 09:36:21 -0700

    fix error in niftiwrite_amd

--- FEMA/niftiwrite_amd.m
+++ FEMA/niftiwrite_amd.m
@@ -6,7 +6,7 @@ if strcmp(fname_nii(max(1,(length(fname_nii)-2)):end),'.gz')
 end
 
 T = M'; T(4,:) =  M*[1 1 1 1]'; Transform = struct('Dimensionality',3,'T',T); 
-info = struct('Transform',Transform,'Datatype',class(vol),'ImageSize',size(vol),'Description','','Version','NIfTI1','Qfactor',1,'PixelDimensions',sum(M(:,1:3).^2,1),'SpaceUnits','Millimeter',...
+info = struct('Transform',Transform,'Datatype',class(vol),'ImageSize',size(vol),'Description','','Version','NIfTI1','Qfactor',1,'PixelDimensions',sum(M(:,1:3).^2,1).^.5,'SpaceUnits','Millimeter',...
               'TimeUnits','Second','SliceCode','Unknown','AdditiveOffset',0,'MultiplicativeScaling',1,'TimeOffset',0,'FrequencyDimension',0,'PhaseDimension',0,'SpatialDimension',0,'DisplayIntensityRange',[0 0],'TransformName','Sform');
 niftiwrite(vol,fname_nii,info,'Compressed',compressedflag);
 
