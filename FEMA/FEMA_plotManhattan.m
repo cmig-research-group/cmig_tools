45d1fb1d49e2d112279e65a43253a59077aafd29
Pravesh Parekh <praveshparekh@gmail.com>
2023-05-09 15:11:47 +0200

    initial commit

---
+++ FEMA/FEMA_plotManhattan.m
@@ -0,0 +1,220 @@
+function fig = FEMA_plotManhattan(fileName, outDir, strTitle, fileFlag, GWASThresh, Chr, bpLoc, logpVals)
+% Function that reads a GWAS summary file and prepares a Manhattan plot
+%% Inputs:
+% fileName:     full path to a GWAS summary file
+%
+% outDir:       full path to where the figure should be saved (if empty,
+%               figure handle is returned and image is not saved)
+%
+% strTitle:     character indicating the title of the figure (if left
+%               empty, no title will be put on top of the figure); this is
+%               also used for deciding output name; if empty, the output
+%               plot name is 'ManhattanPlot.png'; else 'Manhattan_strTitle'
+%
+% fileFlag:     indicate which software was used to generate the GWAS 
+%               summary file (not case sensitive):
+%                   * 'FEMA'    (default)
+%                   * 'GCTA'    (assumes fastGWA output)
+%                   * 'Regenie'
+%                   * 'PLINK'   (assumes PLINK 2)
+% 
+% GWSAThresh:   number indicating the GWAS significance threshold level;
+%               assumes -log10 p value
+%               (default: -log10(5 x 10^-8))
+%
+% The following three inputs can be used instead of a fileName to create a
+% plot from some custom/pre-read GWAS summary file (ignored if fileName is 
+% provided):
+% Chr:          vector of chromosome numbers for m SNPs
+% 
+% bpLoc:        vector of base pair locations for m SNPs
+%
+% logpVals:     vector of -log10(p) values for m SNPs
+%
+%% TO-DO:
+% Detect and handle sex chromosomes
+% Remove fixed number of chromosomes
+% Handle situation where one or more chromosome(s) may be missing
+% Add space between chromosomes
+
+%% Check inputs
+if ~exist('fileName', 'var') || isempty(fileName)
+    % Check if Chr, bpLoc, and pValues exist
+    if (~exist('Chr',      'var') || isempty(Chr))    || ...
+       (~exist('bpLoc',    'var') || isempty(bpLoc))  || ...
+       (~exist('logpVals', 'var') || isempty(logpVals))
+            warning('No relevant inputs provided;, nothing to do'); 
+            return
+    else
+        % Ensure Chr, bpLoc, and pValues have same sizes
+        numSNPs = size(Chr,1);
+        if length(bpLoc) ~= numSNPs || length(logpVals) ~= numSNPs
+            error('Mismatch in size of Chr, bpLoc, and pValue');
+        else
+            readFile = false;
+        end
+    end
+else
+    % Make sure file exists
+    if ~exist(fileName, 'file')
+        error(['Unable to find file: ', fileName]);
+    else
+        readFile = true;
+        % Check fileFlag
+        if ~exist('fileFlag', 'var') || isempty(fileFlag)
+            fileFlag = 'fema';
+        else
+            fileFlag = lower(fileFlag);
+            if ~ismember(fileFlag, {'fema', 'gcta', 'regenie', 'plink'})
+                error('Unknown fileFlag provided');
+            end
+        end
+    end
+end
+
+% Check outDir
+if ~exist('outDir', 'var') || isempty(outDir)
+    writeFile = false;
+else
+    writeFile = true;
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+end
+
+% Check strTitle
+if ~exist('strTitle', 'var') || isempty(strTitle)
+    doTitle  = false;
+else
+    doTitle = true;
+end
+
+% % Check keepNaN
+% if ~exist('keepNaN', 'var') || isempty(keepNaN)
+%     keepNaN = true;
+% else
+%     if ~islogical(keepNaN)
+%         error('keepNaN should be either true or false');
+%     end
+% end
+
+% Check GWASThresh
+if ~exist('GWASThresh', 'var') || isempty(GWASThresh)
+    GWASThresh = -log10(5e-08);
+end
+
+% Prepare output name, if required
+if writeFile
+    if doTitle
+        outName = ['Manhattan_', strrep(strTitle, ' ', ''), '.png'];
+    else
+        outName = 'ManhattanPlot.png';
+    end
+end
+
+%% Read file, if required
+if readFile
+    fileData = readtable(fileName, 'FileType', 'text');
+
+    % Extract relevant information
+    switch fileFlag
+        case 'fema'
+            Chr      = fileData.Chromosome;
+            bpLoc    = fileData.BasePair;
+            logpVals = fileData.log10pValue;
+            
+        case 'gcta'
+            Chr      = fileData.CHR;
+            bpLoc    = fileData.POS;
+            logpVals = -log10(fileData.P);
+            
+        case 'regenie'
+            Chr      = fileData.CHROM;
+            bpLoc    = fileData.GENPOS;
+            logpVals = fileData.LOG10P;
+            
+        case 'plink'
+            % Restrict to rows which are additive tests
+            fileData(~strcmpi(fileData.TEST, 'ADD'), :) = [];
+            Chr      = fileData.x_CHROM;
+            bpLoc    = fileData.POS;
+            logpVals = -log10(fileData.P);
+    end
+    clear fileData
+end
+
+% %% Remove NaN values, if required
+% if ~keepNaN
+%     locNaN           = isnan(logpVals);
+%     [a, b]           = histcounts(categorical(Chr(locNaN)));
+%     Chr(locNaN)      = [];
+%     bpLoc(locNaN)    = [];
+%     logpVals(locNaN) = [];
+%     disp(['Removing ', num2str(sum(locNaN)), ' NaN values']);
+%     info = strcat('Removed', {' '}, cellstr(num2str(a')), ' SNPs from chromosome', {' '}, b');
+%     fprintf('%s \n', info{:});
+% end
+
+%% Create figure
+fig = figure('Units', 'centimeters', 'Position', [10 10 16 14]);
+if doTitle
+    tight_subplot(1, 1, 0, [0.045 0.04], [0.1 0.02]);
+else
+   tight_subplot(1, 1, 0, [0.045 0.01], [0.1 0.02]); 
+end
+ax       = gca;
+offset   = 0;
+numChr   = 22;
+tickLoc  = zeros(numChr,1);
+% spacer  = max(bpLoc(Chr == 1))/5;
+hold(ax, 'on');
+
+% Define colours - 3-class BuPu [colorbrewer2.org]
+colOdd  = [136, 86,  167]./255;
+colEven = [158, 188, 218]./255;
+
+% Do plot
+for chr = 1:numChr
+    % Find locations to plot
+    locs = Chr == chr;
+    
+    % Plot
+    if logical(mod(chr,2))
+        scatter(bpLoc(locs) + offset, logpVals(locs), 'Marker', '.', 'MarkerEdgeColor', colEven, 'SizeData', 45);
+    else
+        scatter(bpLoc(locs) + offset, logpVals(locs), 'Marker', '.', 'MarkerEdgeColor', colOdd,  'SizeData', 45);
+    end
+    
+    % Figure out where to place the tick
+    tickLoc(chr) = offset + ((offset + max(bpLoc(locs))) - (offset + min(bpLoc(locs))))/2;
+
+    % Update offset to the last base pair
+    offset = offset + max(bpLoc(locs));
+end
+
+% Add GWAS line
+plot([0, offset + max(bpLoc(Chr == numChr))], [GWASThresh, GWASThresh], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1.5);
+
+% Adjust X axis settings
+xlim([0, offset + max(bpLoc(Chr == numChr))]);
+xticks(tickLoc);
+xticklabels(1:numChr);
+xtickangle(90);
+ax.XAxis.FontSize = 8;
+
+% Adjust Y axis settings
+ylim([min(logpVals) max(logpVals)+1]);
+ax.YAxis.TickValues = sort([ax.YAxis.TickValues, round(GWASThresh, 2)]);
+ax.YAxis.TickLabelsMode = 'auto';
+ylabel('-log_{10} \itp', 'Interpreter', 'tex', 'FontSize', 12);
+
+% Put a title, if user wants
+if doTitle
+    title(strTitle, 'FontSize', 12);
+end
+
+%% Print figure, if required
+if writeFile
+    print(fullfile(outDir, outName), '-dpng', '-r600');
+    close(fig);
+end
---
+++ FEMA/FEMA_plotQQ.m
@@ -0,0 +1,176 @@
+function fig = FEMA_plotQQ(fileName, outDir, strTitle, fileFlag, pVals)
+% Function that reads a GWAS summary file and creates a QQ plot
+%% Inputs:
+% fileName:     full path to a GWAS summary file
+%
+% outDir:       full path to where the figure should be saved (if empty, 
+%               figure handle is returned and image is not saved)
+%
+% strTitle:     character indicating the title of the figure (if left
+%               empty, no title will be put on top of the figure); this is
+%               also used for deciding output name; if empty, the output
+%               plot name is 'QQPlot.png'; else 'QQ_strTitle'
+%
+% fileFlag:     indicate which software was used to generate the GWAS 
+%               summary file (not case sensitive):
+%                   * 'FEMA'    (default)
+%                   * 'GCTA'    (assumes fastGWA output)
+%                   * 'Regenie'
+%                   * 'PLINK'   (assumes PLINK 2)
+% 
+% The following input can be used instead of a fileName to create a
+% plot from some custom/pre-read GWAS summary file (ignored if fileName is 
+% provided):
+% pVals:        vector of p values (not -log10 values)
+%
+%% Check inputs
+if ~exist('fileName', 'var') || isempty(fileName)
+    % Check if logpVals exist
+    if (~exist('pVals', 'var') || isempty(pVals))
+        warning('No relevant inputs provided;, nothing to do');
+        return
+    else
+        % Sanity check p values
+        minP = min(pVals);
+        maxP = max(pVals);
+        if minP < 0 || maxP > 1
+            error('Possibly incorrect p values; please check data');
+        end
+        readFile = false;
+    end
+else
+    % Make sure file exists
+    if ~exist(fileName, 'file')
+        error(['Unable to find file: ', fileName]);
+    else
+        readFile = true;
+        % Check fileFlag
+        if ~exist('fileFlag', 'var') || isempty(fileFlag)
+            fileFlag = 'fema';
+        else
+            fileFlag = lower(fileFlag);
+            if ~ismember(fileFlag, {'fema', 'gcta', 'regenie', 'plink'})
+                error('Unknown fileFlag provided');
+            end
+        end
+    end
+end
+
+% Check outDir
+if ~exist('outDir', 'var') || isempty(outDir)
+    writeFile = false;
+else
+    writeFile = true;
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+end
+
+% Check strTitle
+if ~exist('strTitle', 'var') || isempty(strTitle)
+    doTitle  = false;
+else
+    doTitle = true;
+end
+
+% Output name, if required
+if writeFile
+    if doTitle
+        outName = ['QQ_', strrep(strTitle, ' ', ''), '.png'];
+    else
+        outName = 'QQPlot.png';
+    end
+end
+
+%% Read file, if required
+if readFile
+    fileData = readtable(fileName, 'FileType', 'text');
+
+    % Extract relevant information
+    switch fileFlag
+        case 'fema'
+            log10pVals  = fileData.log10pValue;
+            pVals       = 10.^-log10pVals;
+            
+        case 'gcta'
+            log10pVals  = -log10(fileData.P);
+            pVals       = 10.^-log10pVals;
+            
+        case 'regenie'
+            log10pVals  = fileData.LOG10P;
+            pVals       = 10.^-log10pVals;
+            
+        case 'plink'
+            % Restrict to rows which are additive tests
+            fileData(~strcmpi(fileData.TEST, 'ADD'), :) = [];
+            pVals       = fileData.P;
+    end
+    clear fileData
+end
+
+%% Create figure
+fig = figure('Units', 'centimeters', 'Position', [10 10 10 10]);
+if doTitle
+    tight_subplot(1, 1, 0, [0.11 0.06], [0.1 0.02]);
+else
+    tight_subplot(1, 1, 0, [0.11 0.01], [0.1 0.02]);
+end
+ax = gca;
+hold(ax, 'on');
+
+% Define colour - 3-class Dark2 [colorbrewer2.org]
+colTrue  = [217, 95, 2]./255;
+
+% Create sorted data
+% Sort p values from lowest to highest
+p_observed = sort(pVals);
+
+% Largest -log10 p value
+maxVal = max(-log10(p_observed));
+
+% Create expected p values
+p_expected = (1:length(p_observed)) / length(p_observed);
+
+% Plot p values
+plot(-log10(p_expected), -log10(p_observed), '.', 'Color', colTrue);
+
+% Plot null
+plot([0, maxVal], [0, maxVal], 'Color', [0 0 0], 'LineStyle', '--', 'LineWidth', 1);
+
+% Round up maxVal
+maxVal = ceil(maxVal);
+
+% Customize axes limits
+xlim([0  maxVal+0.1]);
+ylim([0  maxVal+0.1]);
+
+% Place ticks and tick labels
+if maxVal > 6
+    xticks(0:2:maxVal);
+    yticks(0:2:maxVal);
+    xticklabels(0:2:maxVal);
+    yticklabels(0:2:maxVal);
+else
+    xticks(0:1:maxVal);
+    yticks(0:1:maxVal);
+    xticklabels(0:1:maxVal);
+    yticklabels(0:1:maxVal);
+end
+
+% Change font size for the tick labels
+ax.FontSize = 8;
+
+% Label the axes
+xlabel('Expected -log_{10} \itp', 'Interpreter', 'tex', 'FontSize', 10);
+ylabel('Observed -log_{10} \itp', 'Interpreter', 'tex', 'FontSize', 10);
+
+% Put a title, if user wants
+if doTitle
+    title(strTitle, 'FontSize', 11);
+end
+
+%% Print figure, if required
+if writeFile
+    print(fullfile(outDir, outName), '-dpng', '-r600');
+    close(fig);
+end
