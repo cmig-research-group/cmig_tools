62b25532c5393800b651f5e0e0e9d7d7a27cc122
Diana Smith <dmysmith@gmail.com>
2024-01-23 12:08:49 -0800

    update createBasisNS R function to accept knots, intercept, demean arguments

--- cmig_tools_utils/r/createBasisNS.R
+++ cmig_tools_utils/r/createBasisNS.R
@@ -1,4 +1,4 @@
-createBasisNS <- function(xvec, knots = NULL, df = NULL, intercept = TRUE) {
+createBasisNS <- function(xvec, knots = NULL, df = NULL, intercept = TRUE, demean = TRUE, Boundary.knots = range(xvec), remove_col = NULL) {
   # args      <- commandArgs(trailingOnly = TRUE)
   # splinevar       <- args[1]
   # knots     <- args[2]
@@ -8,11 +8,31 @@ createBasisNS <- function(xvec, knots = NULL, df = NULL, intercept = TRUE) {
   # Get the required library
   require(splines)
 
-  # Create basis functions
-  basis <- data.frame(ns(x=xvec,knots = knots, df=df,intercept = intercept), row.names = xvec)
-  colnames(basis) = paste0('bf_',c(1:df)) 
+  # set default values for knots if not specified
+  if (is.null(knots) & is.null(df)){
+    knots = linspace(min(xvec), max(xvec), 6) 
+    knots = knots[2:(length(knots)-1)] # default will be 5 basis functions with evenly spaced knots
+  }
 
-  return (basis)
+  # Create basis functions
+  basis <- data.frame(ns(xvec, df, knots, intercept, Boundary.knots), row.names = xvec)
+  colnames(basis) = paste0('bf_',c(1:dim(basis)[2]))
+
+  if (demean) {
+    # demean basis functions
+    basis_demeaned = sapply(basis,function(x)x-mean(x))
+    colnames(basis_demeaned) <- paste0("bf_demean_",c(1:dim(basis)[2]))
+    row.names(basis_demeaned) = row.names(basis)
+
+    # resulting matrix is rank deficient; remove middle by default
+    if (is.null(remove_col)){
+      remove_col = round(median(1:dim(basis_demeaned)[2]))
+    }
+    
+    return(basis_demeaned[,-remove_col])
+  } else {
+    return (basis) 
+  }
 }
 
 extract_basis <- function(x, basis) {
@@ -31,7 +51,7 @@ get_basis_values <- function(data, basis, varname){
   basis_values <- lapply(data[,varname], extract_basis, basis=basis)
 
   # Combine basis values as columns in dataframe
-  basis_values_df <- rbindlist(lapply(basis_values, function(x) if(is.null(x)) data.frame(matrix(ncol = dim(basis)[2], nrow = 1)) else x), use.names=FALSE)
+  basis_values_df <- rbindlist(lapply(basis_values, function(x) if(is.null(x[])) data.frame(matrix(ncol = dim(basis)[2], nrow = 1)) else transpose(x)), use.names=FALSE)
 
   return (basis_values_df)
 
