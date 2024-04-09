aeab0d2d06e8c7730c9deccad71a05cc85094a70
Diana Smith <dmysmith@gmail.com>
2024-04-09 12:26:59 -0700

    update fullvol to accept array inputs in addition to vector inputs

--- cmig_tools_utils/matlab/fullvol.m
+++ cmig_tools_utils/matlab/fullvol.m
@@ -1,12 +1,21 @@
 function vol = ABCD_fullvol(volvec,vol_mask)
 
 ivec_mask = find(vol_mask>=0.5);
-nframes = length(volvec)/length(ivec_mask);
+
+if size(volvec,1)>1 
+    nframes = size(volvec,1);
+else
+    nframes = length(volvec)/length(ivec_mask);
+end
 
 vol = zeros([size(vol_mask) nframes],class(volvec));
 for fi = 1:nframes
   vol_tmp = zeros([size(vol_mask) 1],class(volvec));
-  vol_tmp(ivec_mask) = volvec((fi-1)*length(ivec_mask)+[1:length(ivec_mask)]);
+  if size(volvec,1)>1
+      vol_tmp(ivec_mask) = volvec(fi,:);
+  else
+      vol_tmp(ivec_mask) = volvec((fi-1)*length(ivec_mask)+[1:length(ivec_mask)]);
+  end
   vol(:,:,:,fi) = vol_tmp;
 end
 
