21cd0ad4a78356ed61b74f6b6a059b38767b62ec
Ali-Rigby <arigby@health.ucsd.edu>
2024-07-24 18:06:35 -0700

    adding helper functions to utils folder

---
+++ showVol/utils/gotoPointLPH.m
@@ -0,0 +1,4 @@
+function handles = gotoPointLPH(handles, targetLPH)
+    Mlph2vxl = inv(handles.Mvxl2lph_atlas);
+    targetRCS = Mlph2vxl * targetLPH;
+    handles = gotoPointRCS(handles, targetRCS);
