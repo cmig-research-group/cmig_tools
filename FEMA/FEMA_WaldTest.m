f37e92ab05cebeeb1ffdb683860a6c32ff8f7a5c
Ali-Rigby <arigby@health.ucsd.edu>
2024-11-26 07:55:56 -0800

    Updating Wald test to include -log10 p-values

--- FEMA/FEMA_WaldTest.m
+++ FEMA/FEMA_WaldTest.m
@@ -46,6 +46,12 @@ function [W, p, df, L] = FEMA_WaldTest(L, beta_hat, coeffCovar, hypValue, doF, n
 %               null hypothesis that the linear combination of coefficients
 %               are equal to the hypothesised value (or zero)
 %
+% logp:         [k x v] vector of log10 p values, where k is the number of cells
+%               in the input L; if L was numeric, logp is scalar; this is the
+%               logp value under F or chi-squared distribution testing the
+%               null hypothesis that the linear combination of coefficients
+%               are equal to the hypothesised value (or zero)
+%
 % df:           [k x 1] vector of numerator degrees of freedom, where k is
 %               the number of cells in the input L; if L was numeric, df1 
 %               is a scalar; df1 = number of linearly independent rows in
@@ -195,6 +201,8 @@ for cc = 1:numCells
     end
 end
 
+% Calculate -log10 p-values
+logp = -log10(max(1e-300,p));
 
 function L = validateContrast(L, numX)
 % Function that validates and pads a contrast
