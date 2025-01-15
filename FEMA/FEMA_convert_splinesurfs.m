b9cbce55366eeef47a46be15e9fe8b1fe7193827
dpecheva <dpecheva@health.ucsd.edu>
2025-01-14 17:12:43 -0800

    bug fix FEMA_convert_splinesurfs.m

--- FEMA/FEMA_convert_splinesurfs.m
+++ FEMA/FEMA_convert_splinesurfs.m
@@ -14,8 +14,8 @@ function [surf_dbf, varargout] = FEMA_convert_splinesurf(basisSubset, Xvars, bet
 %
 % colnum_intercept: column number of beta_hat corresponding to intercept
 %
-% xvec:         vector of x values over which basis functions are evaluated;
-%               length is equal to number of rows in tbl_bf
+% Xvars:         vector of x values over which basis functions are evaluated;
+%               length is equal to number of rows in basisSubset
 %
 % mask:         binary mask of vertices to be included. 
 %
@@ -25,13 +25,13 @@ function [surf_dbf, varargout] = FEMA_convert_splinesurf(basisSubset, Xvars, bet
 %               file will not be saved
 %
 %% Outputs:
-% surf_dbf:     derivatives of spline estimates for different values of xvec
+% surf_dbf:     derivatives of spline estimates for different values of Xvars
 %
-% semat_dbf:   standard error of the derivative of the spline estimates for each value of xvec
+% semat_dbf:   standard error of the derivative of the spline estimates for each value of Xvars
 %
 % zmat_dbf:    z-scores of the derivative of the spline estimates
 %
-% surf_bf:      the spline estimates (not their derivatives) for different values of xvec
+% surf_bf:      the spline estimates (not their derivatives) for different values of Xvars
 %
 % semat_bf:    the standard error of the spline function itself
 %
@@ -40,7 +40,7 @@ function [surf_dbf, varargout] = FEMA_convert_splinesurf(basisSubset, Xvars, bet
 
 
 %% calculate derivatives of basis functions
-bfmat = table2array(tbl_bf);
+bfmat = table2array(basisSubset);
 
 if ~exist('colnums_bf', 'var')
 	colnums_bf = 1:size(bfmat,2); % Identify X columns / beta coeffcients corresponding to BFs -- this needs to be done in more elegant way
@@ -50,14 +50,14 @@ statmat_dbf = beta_hat(colnums_bf,:); % Select betas for columns of interest -- 
 statmat_bf = beta_hat([colnums_bf colnum_intercept],:);
 ivec_mask = find(mask);
 [dummy dbfmat] = gradient(bfmat); 
-dbfmat = dbfmat/(xvec(2)-xvec(1));
+dbfmat = dbfmat/(Xvars(2)-Xvars(1));
 
 %% compute spline function as weighted sum of betas for basis functions
 
 % gradient of basis functions
-semat_dbf = NaN([length(xvec) size(beta_hat,2)]);
-zmat_dbf = NaN([length(xvec) size(beta_hat,2)]);
-for xi = 1:length(xvec)
+semat_dbf = NaN([length(Xvars) size(beta_hat,2)]);
+zmat_dbf = NaN([length(Xvars) size(beta_hat,2)]);
+for xi = 1:length(Xvars)
     wvec = dbfmat(xi,:)';
     valvec_dbf = sum(statmat_dbf.*wvec,1); 
     sevec_dbf = rowvec(sqrt(pagemtimes(pagemtimes(wvec',coeffCovar(colnums_bf,colnums_bf,:)),wvec)));
@@ -68,9 +68,9 @@ for xi = 1:length(xvec)
 end
 
 % basis functions
-semat_bf = NaN([length(xvec) size(beta_hat,2)]);
-zmat_bf = NaN([length(xvec) size(beta_hat,2)]);
-for xi = 1:length(xvec)
+semat_bf = NaN([length(Xvars) size(beta_hat,2)]);
+zmat_bf = NaN([length(Xvars) size(beta_hat,2)]);
+for xi = 1:length(Xvars)
     wvec = bfmat(xi,:)';
     valvec_bf = sum(statmat_bf.*[wvec; 1],1); 
     sevec_bf = rowvec(sqrt(pagemtimes(pagemtimes([wvec; 1]',coeffCovar([colnums_bf colnum_intercept], [colnums_bf colnum_intercept],:)),[wvec; 1])));
