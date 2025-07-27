182fe4206bc208b77dc977cf5067f6f3570d1462
abnmsry <65160553+abnmsry@users.noreply.github.com>
2025-07-27 23:34:14 +0800

    Update glmmTMB.R

--- Simulation/glmmTMB.R
+++ Simulation/glmmTMB.R
@@ -9,7 +9,7 @@ library(stringr)
 library(R.matlab)
 source("Func.R")
 
-file_path = "./Simulation/DATA/"
+file_path = "./DATA/"
 
 file_list = list.files(path = file_path, full.names = TRUE)
 
@@ -49,13 +49,13 @@ end_time = Sys.time()
 time_taken = end_time - start_time
 print(time_taken)
 
-write.table(glmmTMB_results,"./Simulation/glmmTMB.txt",row.names=F,quote=F,sep="\t")
+write.table(glmmTMB_results,"./glmmTMB.txt",row.names=F,quote=F,sep="\t")
 
 
 #################################################################################
 # Comparison - FEMAlog VS true
 #################################################################################
-CpDat = read.table("./Simulation/FEMA_binary.txt",sep="\t",header=T)
+CpDat = read.table("./FEMA_binary.txt",sep="\t",header=T)
 ########### FE
 diff  = cbind(CpDat[, 5:9] - CpDat[, 13:17])
 colnames(diff) = paste0("BETA",1:5)
@@ -63,7 +63,7 @@ set   = CpDat[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/FEMAb_true_fe_diff.pdf",
+  file = "./FEMAb_true_fe_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -80,7 +80,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/FEMAb_true_fe_rmse.pdf",
+  file = "./FEMAb_true_fe_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -98,7 +98,7 @@ set   = CpDat[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/FEMAb_true_re_diff.pdf",
+  file = "./FEMAb_true_re_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -115,7 +115,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/FEMAb_true_re_rmse.pdf",
+  file = "./FEMAb_true_re_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -131,7 +131,7 @@ dev.off()
 #################################################################################
 # Comparison - glmmTMB VS true
 #################################################################################
-CpDat = read.table("./Simulation/glmmTMB.txt",sep="\t",header=T)
+CpDat = read.table("./glmmTMB.txt",sep="\t",header=T)
 ########### FE
 diff  = cbind(CpDat[, 5:9] - CpDat[, 12:16])
 colnames(diff) = paste0("BETA",1:5)
@@ -139,7 +139,7 @@ set   = CpDat[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/glmmTMB_true_fe_diff.pdf",
+  file = "./glmmTMB_true_fe_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -156,7 +156,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/glmmTMB_true_fe_rmse.pdf",
+  file = "./glmmTMB_true_fe_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -174,7 +174,7 @@ set   = CpDat[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/glmmTMB_true_re_diff.pdf",
+  file = "./glmmTMB_true_re_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -191,7 +191,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/glmmTMB_true_re_rmse.pdf",
+  file = "./glmmTMB_true_re_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -206,8 +206,8 @@ dev.off()
 #################################################################################
 # Comparison - FEMAb VS glmmTMB
 #################################################################################
-FEMAb_results = read.table("./Simulation/FEMA_binary.txt",sep="\t",header=T)%>%arrange(VarFFX,VarFID,r)
-glmmTMB_results = read.table("./Simulation/glmmTMB.txt",sep="\t",header=T)%>%arrange(VarFFX,VarFID,r)
+FEMAb_results = read.table("./FEMA_binary.txt",sep="\t",header=T)%>%arrange(VarFFX,VarFID,r)
+glmmTMB_results = read.table("./glmmTMB.txt",sep="\t",header=T)%>%arrange(VarFFX,VarFID,r)
 ########### FE
 diff  = cbind(FEMAb_results[, 5:9] - glmmTMB_results[, 5:9])
 # diff  = cbind(abs(CpDat[, 1:5]-FEMAb_results[,12:16]) - abs(FEMAb_results[,4:8]-FEMAb_results[, 12:16]))
@@ -216,7 +216,7 @@ set   = FEMAb_results[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/glmmTMB_FEMAb_fe_diff.pdf",
+  file = "./glmmTMB_FEMAb_fe_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -233,7 +233,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/glmmTMB_FEMAb_fe_rmse.pdf",
+  file = "./glmmTMB_FEMAb_fe_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -252,7 +252,7 @@ set   = FEMAb_results[, 1:3]
 plot_dat = cbind(set, diff)
 
 pdf(
-  file = "./Simulation/glmmTMB_FEMAb_re_diff.pdf",
+  file = "./glmmTMB_FEMAb_re_diff.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
@@ -269,7 +269,7 @@ plot_dat = plot_dat %>% group_by(Var_FFX, Var_FID, Var_IID) %>%
 
 
 pdf(
-  file = "./Simulation/glmmTMB_FEMAb_re_rmse.pdf",
+  file = "./glmmTMB_FEMAb_re_rmse.pdf",
   # type = "cairo", # ??????
   # res = 1000, # 300ppi ?ֱ???
   width = 10, height = 10,
