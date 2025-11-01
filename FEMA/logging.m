141d8eb2b0fa5cfa114f3776154f82729a2c0414
Pravesh Parekh <praveshparekh@gmail.com>
2025-10-31 17:21:18 -0700

    bug fix; updated logging file
    Former-commit-id: cec993d80ce1dbfc806b64ebb6a79f942fef037c

--- FEMA/logging.m
+++ FEMA/logging.m
@@ -1,20 +1,19 @@
-function [] = logging(text, varargin)
+function logging(text, varargin)
 %logging - Standard logging utility
 %
-% Syntax: [] = logging(text)
+% Syntax: logging(text)
 %
 % Logs in the following format: 04-Apr-2022 18:28:01 - mfilename - text
 stack = dbstack;
-if length(stack) == 1
+if isscalar(stack)
     current_file = 'running script';
 else
     current_file = stack(2).name;
 end
 
-if length(varargin) == 0
-    fprintf(1, '%s in %s - %s\n', datestr(now()), current_file, text);
+if isempty(varargin)
+    fprintf(1, '%s in %s - %s\n', char(datetime('now')), current_file, text);
 else
-    fprintf(1, '%s in %s - %s\n', datestr(now()), current_file, sprintf(text, varargin{:}));
+    fprintf(1, '%s in %s - %s\n', char(datetime('now')), current_file, sprintf(text, varargin{:}));
 end
-
 end
