169cf35dee31d73f1ac814be9ea975f5927298b0
parekhpravesh <parekhpravesh@users.noreply.github.com>
2026-05-20 01:48:08 +0000

    version info patched
    Co-authored-by: dpecheva <63603013+dpecheva@users.noreply.github.com>
    Co-authored-by: parekhpravesh <praveshparekh@gmail.com>

--- FEMA/FEMA_info.m
+++ FEMA/FEMA_info.m
@@ -11,7 +11,7 @@ else
 end
 
 dirDocs = fullfile(fileparts(fileparts(which('caller_FEMA'))), 'docs');
-version = 'FEMA 4.0.1 (2026.05.18)';
+version = 'FEMA 4.0.2 (2026.05.19)';
 
 switch action
     case {'version', 'ver', 'v'}
